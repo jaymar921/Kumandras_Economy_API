79cf72e34a0d206534d31eaa21f102f3c2cfb41e
JayMar <JayMar@PC-NOT-FOUND>
2021-08-21 08:40:16 +0800

    modified pom.xml and PlayerHeads.java

--- src/main/java/me/jaymar921/kumandraseconomy/ItemHandler/PlayerHeads.java
+++ src/main/java/me/jaymar921/kumandraseconomy/ItemHandler/PlayerHeads.java
@@ -1,51 +1,12 @@
 package me.jaymar921.kumandraseconomy.ItemHandler;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.UUID;
 
 public class PlayerHeads {
-
-    private static ItemStack createSkull(String url, String name, String lore) {
-        @SuppressWarnings("deprecation")
-        ItemStack head = new ItemStack(Material.PLAYER_HEAD, 1, (short) 3);
-        if (url.isEmpty())
-            return head;
-
-        SkullMeta headMeta = (SkullMeta) head.getItemMeta();
-
-        headMeta.setDisplayName(ChatColor.LIGHT_PURPLE+""+ChatColor.BOLD+name);
-        headMeta.setLore(Arrays.asList(ChatColor.GREEN+lore));
-
-        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-
-        profile.getProperties().put("textures", new Property("textures", url));
-
-        try {
-            Field profileField = headMeta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(headMeta, profile);
-
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        head.setItemMeta(headMeta);
-        return head;
-    }
 
     public ItemStack getPlayerHead(Player player){
         ItemStack head = new ItemStack(Material.PLAYER_HEAD,1, (short) 3);
