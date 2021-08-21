8a7253b55ec328b6193b19b7cb45e80cc76348b8
JayMar <JayMar@PC-NOT-FOUND>
2021-08-21 08:23:20 +0800

    modified DeliveryEntity

--- src/main/java/me/jaymar921/kumandraseconomy/entity/DeliveryEntity.java
+++ src/main/java/me/jaymar921/kumandraseconomy/entity/DeliveryEntity.java
@@ -1,6 +1,13 @@
 package me.jaymar921.kumandraseconomy.entity;
 
+import me.jaymar921.kumandraseconomy.KumandrasEconomy;
+
 public class DeliveryEntity {
+
+    KumandrasEconomy main;
+    public DeliveryEntity (KumandrasEconomy plugin){
+        main = plugin;
+    }
 
     public void spawnDeliveryEntity(){
 
